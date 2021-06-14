c8c09339048359c038701e61bdd80d88188f0619
Sergey Ivanenko <storm06kr@gmail.com>
2021-06-14 20:26:07 +0300

    feat: add fragment class

--- app/src/main/java/com/rsschool/quiz/MainActivity.kt
+++ app/src/main/java/com/rsschool/quiz/MainActivity.kt
@@ -2,11 +2,21 @@ package com.rsschool.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        addFragment()
+    }
+
+    private fun addFragment() {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        val quizFragment = QuizFragment()
+        fragmentTransaction.add(R.id.container, quizFragment)
+        fragmentTransaction.commit()
     }
 }
